ae2f52d3d4080f1f39a4ea410fd98f7d72739bb0
mumu <100467197+Jacky088@users.noreply.github.com>
2025-11-24 15:23:42 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { Toaster } from 'vue-sonner'
+// 引入并执行，确保应用启动时主题生效
+import { useTheme } from '@/composables/useTheme'
+useTheme()
 </script>
 
 <template>
   <RouterView />
+  <Toaster position="top-center" />
 </template>
